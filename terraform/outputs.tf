f0a4050d8b1ef027dea56e107da76eb88b53adf0
Paul Aglipay <paglipay@gmail.com>
2024-02-20 10:03:08 -0800

    # output "worker2_public_ip"

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,6 +9,6 @@ output "master_public_ip" {
 output "worker1_public_ip" {
     value = ["${aws_instance.worker1.public_ip}"]
 }
-output "worker2_public_ip" {
-    value = ["${aws_instance.worker2.public_ip}"]
-}
+# output "worker2_public_ip" {
+#     value = ["${aws_instance.worker2.public_ip}"]
+# }
